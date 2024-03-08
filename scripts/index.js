000b492e599684d6f77dd1ee34d61c331b806fce
Riley Marcum <rmarcum13@gmail.com>
2024-03-07 22:24:29 -0500

    project 4, stage 4, submission 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,71 @@
 const initialCards = [
   {
-    name: "controller",
+    name: "Gaming",
     link: "https://images.unsplash.com/photo-1586182987320-4f376d39d787?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "gti",
+    name: "GTI",
     link: "https://images.unsplash.com/photo-1629885389996-c149c822392f?q=80&w=3176&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "pizza",
+    name: "Pizza",
     link: "https://images.unsplash.com/photo-1544982503-9f984c14501a?q=80&w=2667&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "mountains",
+    name: "Mountains",
     link: "https://images.unsplash.com/photo-1434394354979-a235cd36269d?q=80&w=3251&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "redwing",
+    name: "Redwing Blackbird",
     link: "https://images.unsplash.com/photo-1691024305743-9376816b6a92?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "space",
+    name: "Space",
     link: "https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?q=80&w=3313&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
-let profileEditButton = document.querySelector(".profile__edit-button");
-let modal = document.querySelector(".modal");
-let modalCloseButton = modal.querySelector(".modal__close");
+const profile = document.querySelector(".profile");
+const profileEditButton = profile.querySelector(".profile__edit-button");
+const profileName = profile.querySelector(".profile__title");
+const profileJob = profile.querySelector(".profile__description");
+const modal = document.querySelector(".modal");
+const modalForm = modal.querySelector(".modal__form");
+const modalCloseButton = modal.querySelector(".modal__close");
+const nameInput = modalForm.querySelector("input[name='title']");
+const jobInput = modalForm.querySelector("input[name='description']");
+
+for (let i = 0; i < initialCards.length; i++) {
+  getCardElement(initialCards[i]);
+}
+
+function getCardElement(data) {
+  let cardTemplate = document.querySelector("#card").content;
+  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  let cardTitle = cardElement.querySelector(".card__title");
+  let cardImage = cardElement.querySelector(".card__image");
+  cardTitle.textContent = data.name;
+  cardImage.alt = data.name;
+  cardImage.src = data.link;
+  document.querySelector(".cards__list").append(cardElement);
+}
 
 profileEditButton.addEventListener("click", openModal);
 function openModal() {
-  modal.classList.add("modal__opened");
+  modal.classList.toggle("modal__opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+modalForm.addEventListener("submit", handleProfileFormSubmit);
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  modal.classList.toggle("modal__opened");
 }
 
 modalCloseButton.addEventListener("click", closeModal);
 function closeModal() {
-  modal.classList.remove("modal__opened");
+  modal.classList.toggle("modal__opened");
 }
