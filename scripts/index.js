2988d45081efecbda08be045c9fca7e3308841b9
Riley Marcum <rmarcum13@gmail.com>
2024-03-29 04:39:29 -0400

    add form validation, error msg styles

--- scripts/index.js
+++ scripts/index.js
@@ -147,3 +147,64 @@ function addNewImageCard() {
 function deleteImageCard(card) {
   card.closest(".card").remove();
 }
+
+enableValidation();
+
+function enableValidation() {
+  const formList = Array.from(document.forms);
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+}
+
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(".modal__input"));
+  const buttonElement = formElement.querySelector('.modal__button[type="submit"]');
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      toggleButtonState(inputList, buttonElement);
+      toggleInputValidityErrors(formElement, inputElement);
+    });
+  });
+}
+
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("modal__button_inactive");
+  } else {
+    buttonElement.classList.remove("modal__button_inactive");
+  }
+}
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+function toggleInputValidityErrors(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+  inputElement.classList.add("form__input_type_error");
+}
+
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElement.textContent = "";
+  errorElement.classList.remove("form__input-error_active");
+  inputElement.classList.remove("form__input_type_error");
+}
