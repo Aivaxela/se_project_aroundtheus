20209e4048ca8ec0df6c5d0b43ec9665d1930943
Riley Marcum <rmarcum13@gmail.com>
2024-04-09 22:59:39 -0400

    cards are being transferred to Card.js and prepended onto webpage, no event handling functionality yet

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,32 @@
 export default class Card {
-  constructor({ name, link }, cardSelector) {
+  constructor({ name, link }, cardSelector, handleCardImageClick) {
     this._name = name;
     this._link = link;
     this._cardSelector = cardSelector;
+    this._handleCardImageClick = handleCardImageClick;
   }
 
-  _setEventListeners() {}
-
   getView() {
+    this._cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+    this._cardTitle = this._cardElement.querySelector(".card__title");
+    this._cardImage = this._cardElement.querySelector(".card__image");
+    this._cardDeleteIcon = this._cardElement.querySelector(".card__delete-button");
+    this._cardLikeIcon = this._cardElement.querySelector(".card__like-button");
+    this._cardTitle.textContent = this._name;
+    this._cardImage.alt = this._name;
+    this._cardImage.src = this._link;
+
     this._setEventListeners();
+
+    return this._cardElement;
+  }
+
+  _setEventListeners() {
+    this._cardImage.addEventListener("click", () => {
+      this._handleCardImageClick();
+    });
   }
 }
